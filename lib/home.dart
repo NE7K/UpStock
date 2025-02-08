24884a01f5581358f24f02c4dc43a07bf0b324f4
NE7K <sjjang16@naver.com>
2025-02-08 20:32:52 +0900

    Fixed Home Layout

--- lib/home.dart
+++ lib/home.dart
@@ -27,24 +27,38 @@ class _HomeState extends State<Home> {
           children: [
             SizedBox( height: 80 ),
 
-            Text('Stock List', style: TextStyle( fontSize: 30)),
+            Text('Stock List', style: TextStyle( fontSize: 20)),
 
             SizedBox( height: 30 ),
 
             Container(
               padding: EdgeInsets.all(20),
+              margin: EdgeInsets.symmetric(horizontal: 50),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
                 boxShadow: [
-
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 5,
+                    offset: Offset(0, 2)
+                  )
                 ]
               ),
               child: Text('다우존스, 나스닥 100, 러셀 지수 들어가는 곳'),
+            ),
+
+            SizedBox( height: 30 ),
+
+            Text('New Context', style: TextStyle( fontSize: 20)),
+
+            SizedBox( height: 30 ),
+
+            Expanded(
+                child: ListView(
+
+                )
             )
-
-
-
 
 
           ],

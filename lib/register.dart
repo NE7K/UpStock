4fadadee098be45d27e3d67bae8c5e0797c7b4d5
NE7K <sjjang16@naver.com>
2025-02-09 19:21:03 +0900

    Restart Design

--- lib/register.dart
+++ lib/register.dart
@@ -9,157 +9,21 @@ class Register extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        margin: EdgeInsets.all(20),
         width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/images/test.jpg'),
-              fit: BoxFit.cover
-          )
-        ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox( height: 90 ),
-
-                    Text('Stock Comunicatity', style: TextStyle( fontSize: 40, color: Colors.white, fontWeight: FontWeight.w600)),
-                    Text('Register Now', style: TextStyle( fontSize: 20, color: Colors.white)),
-
-                    SizedBox( height: 50 )
-                  ],
+            SizedBox( height: 100 ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                prefixIcon: Icon(Icons.person_outline),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10)
                 )
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only( topLeft: Radius.circular(40), topRight: Radius.circular(40))
-                ),
-
-                child: Container(
-                  margin: EdgeInsets.all(20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox( height: 30 ),
-
-                      Text('Create ID', style: TextStyle( fontSize: 15, color: Colors.grey[600]),),
-
-                      SizedBox( height: 10 ),
-
-                      // Card
-                      Container(
-                        margin: EdgeInsets.symmetric( horizontal: 20),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(30)),
-                            border: Border.all(
-                              color: Colors.grey[200]!
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.grey,
-                                  blurRadius: 5,
-                                  offset: Offset(0, 5)
-                              )
-                            ]
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              hintText: 'Insert Email',
-                              hintStyle: TextStyle( color: Colors.grey ),
-                              border: InputBorder.none
-                          ),
-                        ),
-                      ),
-
-                      // password
-                      SizedBox( height: 30 ),
-
-                      Text('Create Password', style: TextStyle( fontSize: 15, color: Colors.grey[600]),),
-
-                      SizedBox( height: 10 ),
-
-                      // Card
-                      Container(
-                        margin: EdgeInsets.symmetric( horizontal: 20 ),
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.all(Radius.circular(30)),
-                            border: Border.all(
-                                color: Colors.grey[200]!
-                            ),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.grey,
-                                  blurRadius: 5,
-                                  offset: Offset(0, 5)
-                              )
-                            ]
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              hintText: 'Insert Password',
-                              hintStyle: TextStyle( color: Colors.grey ),
-                              border: InputBorder.none
-                          ),
-                        ),
-                      ),
-
-                      SizedBox( height: 20 ),
-
-
-                      Column(
-                        children: [
-                          Text('I forgot my Email', style: TextStyle( fontSize: 12, color: Colors.grey)),
-
-                          SizedBox( height: 50 ),
-
-                          Container(
-                              height: 60,
-                              margin: EdgeInsets.symmetric(horizontal: 60),
-                              decoration: BoxDecoration(
-                                  color: Colors.orange[400],
-                                  borderRadius: BorderRadius.circular(50)
-                              ),
-                              child: Center(
-                                child: TextButton(onPressed: () {}, child: Text('Register', style: TextStyle( color: Colors.white, fontSize: 20)))
-                              )
-                          ),
-
-                          SizedBox( height: 15 ),
-
-                          Container(
-                              height: 60,
-                              margin: EdgeInsets.symmetric(horizontal: 60),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(50),
-                                  border: Border.all(
-                                      color: Colors.orange[600]!,
-                                      width: 2
-                                  )
-                              ),
-                              child: Center(
-                                child: TextButton(onPressed: () {
-                                  Navigator.pop(context);
-                                }, child:  Text('Done', style: TextStyle( color: Colors.grey[900]!, fontSize: 20)))
-                              )
-                          )
-
-                        ],
-                      )
-                    ],
-                  ),
-                ),
               ),
-            )
+
+            ),
           ],
         ),
       )

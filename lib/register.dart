02efe755a0b1cce5021bde3b83dc0b799a2fae45
NE7K <sjjang16@naver.com>
2025-02-09 19:50:53 +0900

    Fixed register design

--- lib/register.dart
+++ lib/register.dart
@@ -3,7 +3,11 @@ import 'package:upstock/home.dart';
 import 'package:upstock/main.dart';
 
 class Register extends StatelessWidget {
-  const Register({super.key});
+  Register({super.key});
+
+  // TODO : MediaQuery 사용해서 동적 UI 제공. 사진 포함
+  // TODO : context도 main.dart에서 가져와야 함
+  // var screenWidth = MediaQuery.of(context).size.width;
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +18,76 @@ class Register extends StatelessWidget {
         child: Column(
           children: [
             SizedBox( height: 100 ),
+
+            Image.asset('assets/images/icon.png',
+                width: 300,
+                height: 300,
+            ),
+
+            SizedBox( height: 50 ),
+
+            Text('Welcome to app "UpStock"',
+                style: TextStyle(
+                  fontSize: 20
+                )
+            ),
+
+            SizedBox( height: 20 ),
+
             TextField(
               decoration: InputDecoration(
                 labelText: 'Email',
-                prefixIcon: Icon(Icons.person_outline),
+                hintText: 'Insert your e-mail',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400
+                ),
+
+                prefixIcon: Icon(Icons.person_outline), // icon images
+
                 enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade200, width: 1.5),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                floatingLabelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 1.5),
                   borderRadius: BorderRadius.circular(10)
                 )
               ),
-
             ),
+
+            SizedBox(height: 20),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                hintText: 'Insert your Password',
+                labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400
+                ),
+                
+                prefixIcon: Icon(Icons.vpn_key_outlined),
+
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade200, width: 1.5),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade200, width: 1.5),
+                  borderRadius: BorderRadius.circular(10)
+                )
+
+              ),
+            )
+
           ],
         ),
       )

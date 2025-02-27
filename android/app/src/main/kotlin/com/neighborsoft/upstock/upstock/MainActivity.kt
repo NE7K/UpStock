82658cc96a8efdd76203cd9deb5a84a3618237d0
NE7K <sjjang16@naver.com>
2025-02-27 16:22:02 +0900

    Removed ignored files from Git tracking

--- android/app/src/main/kotlin/com/neighborsoft/upstock/upstock/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.neighborsoft.upstock.upstock
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/style/style.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-var theme = ThemeData(
-    splashColor: Colors.transparent, // 전역 스플래시 색상
-    highlightColor: Colors.transparent, // 전역 하이라이트 색상
-
-    appBarTheme: AppBarTheme(color: Colors.white),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        selectedItemColor: Colors.black, backgroundColor: Colors.white));

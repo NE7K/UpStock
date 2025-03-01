3f383ab704b249cb857e46abae7bebc08e070f79
NE7K <sjjang16@naver.com>
2025-03-01 15:32:24 +0900

    Reset Layout

--- lib/home/homebody.dart
+++ lib/home/homebody.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:upstock/profile/announcement.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:shimmer/shimmer.dart'; // 차트 임포트임 없으면 차트 못 그림ㅋㅋ
 
 final auth = FirebaseAuth.instance;
 final storage = FirebaseStorage.instance;
@@ -17,15 +18,25 @@ class HomeBody extends StatefulWidget {
 }
 
 class _HomeBodyState extends State<HomeBody> {
-
   // 내용 저장
   List<Map<String, dynamic>> usercontext = [];
+
   // 이미지 변수고 이미지 url 여기다가 다 처 넣을거임 ? = null이 아니라는 것을 보장
   List<String?> imageUrl = [];
 
+  // 사진 및 게시글 로딩 중
+  bool isLoading = false;
+
   // 글 내용 불러오기
   getData() async {
-    var result = await firestore.collection('user').orderBy('date', descending: true).get();
+    setState(() {
+      isLoading = true;
+    });
+
+    var result = await firestore
+        .collection('user')
+        .orderBy('date', descending: true)
+        .get();
     // todo timestamp add oo?
 
     List<Map<String, dynamic>> result2 = [];
@@ -44,6 +55,7 @@ class _HomeBodyState extends State<HomeBody> {
     setState(() {
       usercontext = result2;
       imageUrl = imageUrlresult;
+      isLoading = false;
     });
   }
 
@@ -66,80 +78,109 @@ class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
     return SliverList(
-        delegate: SliverChildBuilderDelegate(
-            childCount: usercontext.length, (c, i) {
+        delegate:
+            SliverChildBuilderDelegate(childCount: usercontext.length, (c, i) {
       return SizedBox(
-        width: double.infinity,
-        child: Column(
-          children: [
+          width: double.infinity,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 10),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(
+                            'https://ne7k.github.io/app/profile.jpg')),
 
-            Container(
-              padding: EdgeInsets.only(left: 10, top: 10),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                      radius: 20,
-                      backgroundImage: NetworkImage('https://ne7k.github.io/app/profile.jpg')),
+                    SizedBox(width: 15),
 
-                  SizedBox(width: 15),
-
-                  // todo 글쓴이 사용자 이름으로 바꿔야함
-                  Text(usercontext[i]['username'] ?? 'name', style: TextStyle( fontSize: 16, fontWeight: FontWeight.bold )),
-                ],
-              ),
-            ),
-
-            SizedBox(height: 10),
-
-            buildImageNotEmpty(imageUrl[i]),
-
-            SizedBox( height: 20 ),
-
-            Row(
-              children: [
-                SizedBox( width: 20 ),
-                // 유저 아이디
-                Text(usercontext[i]['username'] ?? 'name', style: TextStyle( fontSize: 14, fontWeight: FontWeight.bold)),
-                SizedBox( width: 10 ),
-                Text('${usercontext[i]['context']}',
-                    style: TextStyle( fontSize: 14 )
+                    // todo 글쓴이 사용자 이름으로 바꿔야함
+                    Text(usercontext[i]['username'] ?? 'name',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                  ],
                 ),
-              ],
-            ),
-
-            Container(
-              padding: EdgeInsets.symmetric( horizontal: 5 ),
-              child: Column(
+              ),
+              SizedBox(height: 10),
+              buildImageNotEmpty(imageUrl[i]),
+              SizedBox(height: 20),
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {}, icon: Icon(Icons.favorite_outline)),
-                      Text(usercontext[i]['like'].toString()),
-                      // 댓글
-                      // IconButton(
-                      //     onPressed: () {}, icon: Icon(Icons.messenger_outline)),
-                      // Text('2')
-                    ],
-                  ),
-
-                  // 작성 시간
-                  Text(usercontext[i]['date'].toString(), style: TextStyle( color: Colors.black, fontWeight: FontWeight.bold)),
-
-                  // todo timestamp date 객체로 변환해서 사용해야 하는 것 같음. 변환해서 사용하자
+                  SizedBox(width: 20),
+                  // 유저 아이디
+                  Text(usercontext[i]['username'] ?? 'name',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+                  SizedBox(width: 10),
+                  Text('${usercontext[i]['context']}',
+                      style: TextStyle(fontSize: 14)),
                 ],
               ),
-            )
-          ],
-        ),
-      );
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.favorite_outline)),
+                        Text(usercontext[i]['like'].toString()),
+                        // 댓글
+                        // IconButton(
+                        //     onPressed: () {}, icon: Icon(Icons.messenger_outline)),
+                        // Text('2')
+                      ],
+                    ),
+
+                    // 작성 시간
+                    Text(usercontext[i]['date'].toString(),
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold)),
+
+                    // todo timestamp date 객체로 변환해서 사용해야 하는 것 같음. 변환해서 사용하자
+                  ],
+                ),
+              )
+            ],
+          ));
     }));
   }
 }
 
 // 이미지 널이면 빈텍스트 보여줘야함 ㅋㅋㅋㅋ
 Widget buildImageNotEmpty(String? imageUrl) {
-  return imageUrl != null
-      ? Image.network(imageUrl)
-      : SizedBox();
+  return imageUrl != null ? Image.network(imageUrl) : SizedBox();
+}
+
+class ShimmerEffect extends StatelessWidget {
+  const ShimmerEffect({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        padding: EdgeInsets.only(left: 10, right: 10),
+        // 카드뷰
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5)),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
+    ]);
+  }
 }

a9818a0a0094e404476242d8099dcaa49b50c042
NE7K <sjjang16@naver.com>
2025-03-02 15:14:49 +0900

    Added Shimmer homebody

--- lib/home/homebody.dart
+++ lib/home/homebody.dart
@@ -229,28 +229,134 @@ class ShimmerEffect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        padding: EdgeInsets.only(left: 10, right: 10),
-        // 카드뷰
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Shimmer.fromColors(
-                  baseColor: Colors.grey[300]!,
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              SizedBox(width: 10),
+              CircleAvatar(
+                child: Shimmer.fromColors(
+                  baseColor: Colors.grey[200]!,
                   highlightColor: Colors.grey[100]!,
                   child: Container(
                     decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(5)),
+                        borderRadius: BorderRadius.circular(20)),
                   ),
-                )
-              ],
-            )
-          ],
-        ),
-      )
-    ]);
+                ),
+              ),
+              SizedBox(width: 10),
+              Shimmer.fromColors(
+                  baseColor: Colors.grey[200]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    width: 100,
+                    height: 20,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                  )),
+            ],
+          ),
+
+          SizedBox(height: 10),
+
+          Shimmer.fromColors(
+            baseColor: Colors.grey[200]!,
+            highlightColor: Colors.grey[100]!,
+            child: Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                // borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+
+          Row(
+            children: [
+              SizedBox(width: 10),
+              Shimmer.fromColors(
+                baseColor: Colors.grey[200]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  width: 70,
+                  height: 20,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              ),
+              SizedBox(width: 10),
+              Shimmer.fromColors(
+                baseColor: Colors.grey[200]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  width: 300,
+                  height: 20,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              )
+            ],
+          ),
+
+          SizedBox( height: 10 ),
+
+          Row(
+            children: [
+              SizedBox( width: 10),
+
+              Shimmer.fromColors(
+                baseColor: Colors.grey[200]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  width: 40,
+                  height: 20,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+
+              )
+            ],
+          ),
+
+          SizedBox( height: 10 ),
+
+          Row(
+            children: [
+
+              SizedBox( width: 10 ),
+
+              Icon(Icons.favorite_outline),
+
+              SizedBox( width: 5),
+
+              Shimmer.fromColors(
+                baseColor: Colors.grey[200]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  width: 40,
+                  height: 20,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              )
+
+            ],
+          )
+
+        ],
+      ),
+    );
   }
+
 }
